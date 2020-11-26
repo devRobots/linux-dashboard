0ce8feac20e3846421a3dacfe43410ab7a7872d0
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-11-25 23:37:55 -0500

    Pequeño ajuste de la invocacion de scripts en los fragmentos php

--- pages/usuarios.php
+++ pages/usuarios.php
@@ -66,8 +66,6 @@
 	</div>
 
 	<!-- PHP calls -->
-	<?php exec("../scripts/ram.py") ?>
-	<?php exec("../scripts/cpu.py") ?>
 
 	<!-- Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
